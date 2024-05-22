e02d8064d0d235090e9f060e5cc60b68e18004b8
Gourob Mazumder <gourob.mazumder@enosisbd.com>
2024-05-22 21:36:53 +0600

    Initial Commit

---
+++ Crypto-CoinGecko/Crypto_CoinGeckoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_CoinGeckoApp.swift
+//  Crypto-CoinGecko
+//
+//  Created by Gourob Mazumder on 22/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_CoinGeckoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
